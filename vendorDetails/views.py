15aae444621c3552291804c29830c0151ec11492
NAMRATHA S KHASNIS <namratha.khasnis25@gmail.com>
2020-10-29 01:53:32 +0530

    added modified templates to display objects

--- vendorDetails/views.py
+++ vendorDetails/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
+from .models import VendorProfile
+from django.utils import timezone
 # Create your views here.
 
 def post_list(request):
-    return render(request, 'vendorDetails/post_list.html', {})
+    profiles=VendorProfile.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
+    return render(request, 'vendorDetails/post_list.html', {'profiles': profiles})
